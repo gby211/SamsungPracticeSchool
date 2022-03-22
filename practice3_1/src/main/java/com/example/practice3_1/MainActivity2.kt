8e3a6389dc948ac2181474d798657d26fd7cca15
TTawtet <gby211@yandex.ru>
2022-03-22 16:16:41 +0300

    done3_1

--- practice3_1/src/main/java/com/example/practice3_1/MainActivity2.kt
+++ practice3_1/src/main/java/com/example/practice3_1/MainActivity2.kt
@@ -1,8 +1,10 @@
 package com.example.practice3_1
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -18,17 +20,46 @@ class MainActivity2 : AppCompatActivity() {
         var firstValue = intent.getIntExtra(FIRST_VALUE, 0)
         var secondValue = intent.getIntExtra(SECOND_VALUE, 0)
 
-        textView4.text = "ваше чило больше ${firstValue+(secondValue - firstValue)/2}?"
+        textView4.text = "ваше чило больше ${averageNumber(firstValue, secondValue)}?"
 
         button.setOnClickListener {
-            secondValue -= (secondValue - firstValue)/2
-            textView4.text = "ваше чило больше ${firstValue+(secondValue - firstValue)/2}?"
+            secondValue -= (secondValue - firstValue) / 2
+            if (isYouNumber(firstValue, secondValue)) {
+                textView4.text = "ВАШЕ ЧИСЛО - ${averageNumber(firstValue, secondValue) - 1}!"
+                allInvisible()
+            } else
+                textView4.text = "ваше чило больше ${averageNumber(firstValue, secondValue)}?"
+
         }
         button2.setOnClickListener {
-            firstValue += (secondValue - firstValue)/2
-            textView4.text = "ваше чило больше ${firstValue+(secondValue - firstValue)/2}?"
+            firstValue += (secondValue - firstValue) / 2
+            if (isYouNumber(firstValue, secondValue)) {
+                textView4.text = "ВАШЕ ЧИСЛО - ${averageNumber(firstValue, secondValue)}!"
+                allInvisible()
+            } else
+                textView4.text = "ваше чило больше ${averageNumber(firstValue, secondValue)}?"
+
         }
 
+        button4.setOnClickListener {
+            textView4.text = "ВАШЕ ЧИСЛО ${averageNumber(firstValue, secondValue)}!"
+            allInvisible()
+        }
+    }
 
+    private fun averageNumber(firstValue: Int, secondValue: Int): Int {
+        val rangeNumber = secondValue - firstValue
+        return secondValue - rangeNumber / 2
+    }
+
+    private fun isYouNumber(firstValue: Int, secondValue: Int): Boolean {
+        return secondValue - firstValue <= 1
+    }
+
+    private fun allInvisible() {
+        button.visibility = View.INVISIBLE
+        button2.visibility = View.INVISIBLE
+        button4.visibility = View.INVISIBLE
+        textView4.textSize = 22F
     }
 }
