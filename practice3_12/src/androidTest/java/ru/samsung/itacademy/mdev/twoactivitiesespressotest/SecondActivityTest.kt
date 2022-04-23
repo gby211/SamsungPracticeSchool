e6c7dbabb16a6bce131d29ea5755de597d309462
TTawtet <gby211@yandex.ru>
2022-04-23 16:36:56 +0300

    3.12 done

--- practice3_12/src/androidTest/java/ru/samsung/itacademy/mdev/twoactivitiesespressotest/SecondActivityTest.kt
+++ practice3_12/src/androidTest/java/ru/samsung/itacademy/mdev/twoactivitiesespressotest/SecondActivityTest.kt
@@ -1,31 +1,33 @@
 package ru.samsung.itacademy.mdev.twoactivitiesespressotest
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
 class SecondActivityTest {
 
+
     @Rule
     @JvmField
-    var activityRule = ActivityTestRule<MainActivity>(
-        MainActivity::class.java
-    )
-
+    var activityRule = ActivityScenarioRule(SecondActivity::class.java)
+    val ggwp = "test GGWP"
+    
+    
+    
     @Test
-    fun returnReply() {
+    fun messageIsDisplayed(){
+        onView(withId(R.id.editText_second)).check(matches(withText("")))
+        onView(withId(R.id.editText_second)).perform(ViewActions.typeText(ggwp))
+        onView(withId(R.id.editText_second)).check(matches(withText(ggwp)))
     }
-
+    
     @Test
-    @Throws(Exception::class)
-    fun clickLoginButton() {
-        onView(withId(R.id.button_main))
-        onView(withId(R.id.button_main)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_main)).perform(click())
+    fun buttonIsClicked(){
+        onView(withId(R.id.button_second)).perform(ViewActions.click())
     }
 
 }
